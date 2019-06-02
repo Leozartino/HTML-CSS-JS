fb4bccbac0594b2450a2074991103d2836733a01
Leozartino <leozartino@gmail.com>
2019-06-02 11:33:46 -0300

    adcionando exemplinhos

--- first-web-site/scripts/index.js
+++ first-web-site/scripts/index.js
@@ -1,4 +1,28 @@
 const $myButton = document.querySelector("button");
 const $nomePessoa = prompt("Olá, qual é o seu nome?");
+const $num1 = prompt("Digite um número:");
+const $num2 = prompt("Digite outro número:");
 
-$myButton.onclick = () => alert(`Olá, ${$nomePessoa}!`);
+const soma = Number($num1) + Number($num2);
+
+alert("A soma dos valores é: " + soma);
+alert(`Seja bem vindo, ${$nomePessoa}`);
+
+$myButton.onclick = () => alert(`Olá, como você está ${$nomePessoa}?`);
+
+/* let num1 = 0;
+const arr = [0, 5, 6, 7, 8];
+
+//num1 recebe cada elemento do array
+for (num1 of arr) {
+  num1 = num1 + 1;
+  console.log(num1);
+} 
+const objeto = { a: 1, b: 2, c: 3 };
+
+//elem recebe cada numeração do indice do array
+//elem recebe cada chave do objeto
+for (let elem in objeto) {
+  console.log(`chave: ${elem}\nvalor: ${objeto[elem]}`);
+}
+ */
